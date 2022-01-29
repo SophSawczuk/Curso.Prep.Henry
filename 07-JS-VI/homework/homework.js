423ca3ef7aab2f74e638e51379f7cf74f2eda232
SophSawczuk <sofiasawczuka@gmail.com>
2022-01-28 21:44:07 -0300

    Tarea JSVI, segundo intento

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,7 +4,7 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  
+  return nombre[0].toUpperCase() + nombre.slice(1)
 }
 
 function invocarCallback(cb) {
@@ -25,8 +25,8 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-  const suma = numeros.reduce(function(suma, int) {
-    suma = suma + int
+  var suma = numeros.reduce(function(suma, int) {
+    return suma + int
   }, 0) 
   cb(suma)
 }
@@ -35,7 +35,7 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  for(i = 0; i < array.lenght; i++) {
+  for(i = 0; i < array.length; i++) {
     cb(array[i])
   }
 }
@@ -45,22 +45,25 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  const array = [a, b, c, d, e]
+  
   const NuevoArray = array.map(function(elemento) {
-    cb(elemento)
+    return cb(elemento)
   })
+  return NuevoArray
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  const filtro = array.map(function(elemento) {
-    if(elemento === a) {
-      delete elemento
+  
+  var NuevoArray = []
+  for(i = 0; i < array.length; i++) {
+    if(array[i][0] === "a") {
+      NuevoArray.push(array[i])
     }
-  })
-  return filtro
+  }
+  return NuevoArray
 }
 
 // No modificar nada debajo de esta línea
